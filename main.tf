9506787d42df2a6a8e46917e11dece62ffdd1937
Mekala Siva Sankar <dbasivasankar@gmail.com>
2022-05-20 20:08:09 +0530

    first commit

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c79778a2336c68464aa53a5ba05e8e2d725f138ffb2b603435606facde912c23
+oid sha256:4c9e155458b3f26519d8e27d75bf8858850655ebf8cc3a43d825dc4becc65a73
 size 3276
